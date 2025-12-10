026f45ae42c7dcdf0c02efdae8cb17ea25b4a32d
LiquidKang <41722397+LiquidKang@users.noreply.github.com>
2025-12-10 17:24:50 +0800

    Update XbotEX.py

--- XbotEX.py
+++ XbotEX.py
@@ -1,27 +1,21 @@
+import os
 import discord
 import re
-import os   # ← 你缺少這個，Railway 會直接爆掉
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("TOKEN")  # 這裡不用自己寫 Token
 
 intents = discord.Intents.default()
-intents.message_content = True  # 讀取訊息必須開啟
+intents.message_content = True
 client = discord.Client(intents=intents)
 
-# Regex：識別 X.com 的連結
 x_url_pattern = re.compile(r"https?://(www\.)?x\.com/[\w\-/?=&%]+")
 
 @client.event
 async def on_message(message):
-    # 避免 bot 重複回應自己
     if message.author == client.user:
         return
-
-    # 如果訊息包含 x.com
-    if "x.com" in message.content.lower():
+    if "x.com" in message.content:
         new_msg = message.content.replace("x.com", "vxtwitter.com")
         await message.channel.send(new_msg)
 
 client.run(TOKEN)
-
-
